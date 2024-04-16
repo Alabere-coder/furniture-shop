b24e4ade508e5586a93d329260aea31172f9248d
Alabere-coder <alabere4real247@email.com>
2024-04-16 17:26:25 +0100

    faq component added

--- app/components/faq.tsx
+++ app/components/faq.tsx
@@ -12,10 +12,8 @@ const CollapseItem = Collapse.Item;
 
 const Faq = () => {
   return (
-    <div className="flex flex-col gap-4 justify-center items-center max-md:w-[90%] m-auto my-10">
-      <p className="text-4xl text-center max-md:text-3xl">
-        Frequently Asked Questions
-      </p>
+    <div className="flex flex-col gap-4 items-center my-10">
+      <p className="text-4xl max-md:text-3xl">Frequently Asked Questions</p>
       <Collapse accordion style={{ maxWidth: 1180 }}>
         <CollapseItem header="Do you manufacture all your furniture?" name="1">
           Yes, we do. We operate 2 state of the art factories in Mushin, Lagos.
@@ -23,76 +21,39 @@ const Faq = () => {
           consoles, tables, etc Bishop Aggey factory houses our upholstery,
           carpentry, and spray department.
         </CollapseItem>
-        <CollapseItem header="Beijing Toutiao Technology Co., Ltd." name="2">
-          Beijing Toutiao Technology Co., Ltd.
+        <CollapseItem header="When is my order confirmed?" name="2">
+          Your order is accepted when you make a deposit of 80% of the invoice.
+          The balance of 20% will be paid after production, i.e., the items will
+          be fully paid for before they are picked up from the factory. You can
+          also make 100% payment if you wish. Yes. It comes with default styles
+          that matches the other components&apos; aesthetic.
         </CollapseItem>
-        <CollapseItem header="Beijing Toutiao Technology Co., Ltd." name="3">
-          Beijing Toutiao Technology Co., Ltd.
+        <CollapseItem
+          header="How long does it take to process orders?"
+          name="3"
+        >
+          Depending on the type of product and quantity you ordered, our
+          products are manufactured within 1 week upwards. Please make sure you
+          chat with our representative for detailed information about the
+          product you are ordering. You can do this via WhatsApp.
+        </CollapseItem>
+        <CollapseItem
+          header="Can I order my own specifications and dimensions?"
+          name="4"
+        >
+          Yes, you can. Always share your specifications and dimensions with our
+          representative on WhatsApp before ordering.
+        </CollapseItem>
+        <CollapseItem header="How do I know which fabric I'm picking?" name="5">
+          We have a range of various fabrics to choose from. Our representative
+          is always available to help you choose fabric that suits best with
+          your needs. We will send you samples via WhatsApp if necessary.
+        </CollapseItem>
+        <CollapseItem header="Where is your representative?" name="6">
+          Click on the WhatsApp Icon at the bottom right of your screen to chat
+          with our representative. Or call us on this number +2348022233334444
         </CollapseItem>
       </Collapse>
-      {/* <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
-          <AccordionTrigger>
-            Do you manufacture all your furniture?
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes, we do. We operate 2 state of the art factories in Mushin,
-            Lagos. Iyalla factory is for all our board work: doors, kitchen,
-            wardrobes, consoles, tables, etc Bishop Aggey factory houses our
-            upholstery, carpentry, and spray department.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>When is my order confirmed?</AccordionTrigger>
-          <AccordionContent>
-            Your order is accepted when you make a deposit of 80% of the
-            invoice. The balance of 20% will be paid after production, i.e., the
-            items will be fully paid for before they are picked up from the
-            factory. You can also make 100% payment if you wish.
-            {/* Yes. It comes with default styles that matches the other
-            components&apos; aesthetic. 
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger>
-            How long does it take to process orders?
-          </AccordionTrigger>
-          <AccordionContent>
-            Depending on the type of product and quantity you ordered, our
-            products are manufactured within 1 week upwards. Please make sure
-            you chat with our representative for detailed information about the
-            product you are ordering. You can do this via WhatsApp.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-4">
-          <AccordionTrigger>
-            Can I order my own specifications and dimensions?
-          </AccordionTrigger>
-          <AccordionContent>
-            Yes, you can. Always share your specifications and dimensions with
-            our representative on WhatsApp before ordering.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-5">
-          <AccordionTrigger>
-            How do I know which fabric I'm picking?
-          </AccordionTrigger>
-          <AccordionContent>
-            We have a range of various fabrics to choose from. Our
-            representative is always available to help you choose fabric that
-            suits best with your needs. We will send you samples via WhatsApp if
-            necessary.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-6">
-          <AccordionTrigger>Where is your representative?</AccordionTrigger>
-          <AccordionContent>
-            Click on the WhatsApp Icon at the bottom right of your screen to
-            chat with our representative. Or call us on this number
-            +2348022233334444
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion> */}
     </div>
   );
 };
